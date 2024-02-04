e84acbfdce8e397d32761cbbbfd3dbfffc6a7c16
Benoit Queyron <queyronbenoit@gmail.com>
2024-02-04 17:48:12 +0100

    ex done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,35 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Flat.create!(
+  name: 'Le France',
+  adress: 'Paris, rue de la paix',
+  description: 'A lovely summer feel for this spacious garden flat',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Lespagne',
+  adress: 'Madrid, rue des bonheurs',
+  description: 'Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 175,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Langletter',
+  adress: '10 Clifton Gardens London W9 1DT',
+  description: 'Large kitchen and 2 beds, au top',
+  price_per_night: 25,
+  number_of_guests: 13
+)
+
+Flat.create!(
+  name: 'Univers',
+  adress: 'Mars, rue des croisières',
+  description: 'A lovely something in nowhere',
+  price_per_night: 375,
+  number_of_guests: 1
+)
